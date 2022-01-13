95700227b36d5028c9fb9b3a5575cd49b20b1c74
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-12 22:14:33 -0500

    :: Bank of React ::

---
+++ bank-of-react/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+    
+    function Home(props) {
+        return (
+            <div>
+              <img src="https://res.cloudinary.com/andreahabib/image/upload/v1642026304/React_Bank_dk7n1a.png" alt="bank"/>
+              <h1>Bank of React</h1>
+            </div>
+        );
+      }
+    
+    export default Home;
